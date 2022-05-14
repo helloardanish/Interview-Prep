072afe1ff1872ceb9f2d574cad1f3d7c53d3a3a5
A R DANISH <helloardanish@gmail.com>
2022-05-14 15:32:13 +0530

    Update Fibonacci_recursive.cpp

--- Basic-Problems/Fibonacci_recursive.cpp
+++ Basic-Problems/Fibonacci_recursive.cpp
@@ -35,3 +35,24 @@ int main(){
   }
   cout <<endl;
 }
+
+
+
+#include <iostream>
+
+using namespace std; 
+int fibonacci(int n){
+  if(n<0) return -1;
+  if(n==0) return 0;
+  if(n==1) return 1;
+  int small1 = fibonacci(n-1);
+  int small2 = fibonacci(n-2);
+  return small1+small2;
+}
+int main(){ 
+  int n=20;
+  for(int i=0; i<n;i++){
+    cout << fibonacci(i) << " ";
+  }
+  cout <<endl;
+}
