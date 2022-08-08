3d0314161c9c936ff152d0910bb650f76e700f43
A R DANISH <helloardanish@gmail.com>
2022-08-08 22:29:02 +0530

    Update 003_Max_Area_of_Island.cpp

--- Graph/003_Max_Area_of_Island.cpp
+++ Graph/003_Max_Area_of_Island.cpp
@@ -51,3 +51,56 @@ public:
 Runtime: 35 ms, faster than 36.07% of C++ online submissions for Max Area of Island.
 Memory Usage: 23.2 MB, less than 87.05% of C++ online submissions for Max Area of Island.
 */
+
+
+
+// DFS other way
+
+class Solution {
+public:
+    int countConnectedIslands(vector<vector<int>> &grid, int r, int c){
+        //base case
+        if(r>=0 && c>=0 && r<grid.size() && c<grid[0].size() && grid[r][c]==1){
+            
+            //update island
+            grid[r][c] = 0;
+
+            // top bottom left right
+
+            return 1+
+                countConnectedIslands(grid, r-1, c)+
+                countConnectedIslands(grid, r+1, c)+
+                countConnectedIslands(grid, r, c-1)+
+                countConnectedIslands(grid, r, c+1);
+        }
+        
+        return 0;
+    }
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        if(grid.size()==0) return 0;
+        int ans=0;
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                //if current place is island >> 1
+                if(grid[i][j]==1){
+                    //count the connected islands >> 1
+                    int temp = countConnectedIslands(grid, i, j);
+                    //update the connected island value if larger island found
+                    if(temp > ans) ans=temp;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+/*
+Runtime: 20 ms, faster than 85.49% of C++ online submissions for Max Area of Island.
+Memory Usage: 23.2 MB, less than 61.64% of C++ online submissions for Max Area of Island.
+*/
+
+
+// Happy Coding
+// A R
