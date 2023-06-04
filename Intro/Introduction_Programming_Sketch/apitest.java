b5a65c68a0fdf3fad39a609368b7901befc5be20
A R DANISH <helloardanish@gmail.com>
2023-06-05 00:41:20 +0530

    Add files via upload

---
+++ Intro/Introduction_Programming_Sketch/apitest.java
@@ -0,0 +1,29 @@
+class apitest{
+
+@Path("/bankDetais")
+
+/*
+{
+    name:"A R",
+    username : "helloarda"
+}
+*/
+
+public UserDetails abcdfwkgn(String username){
+    UserDetails user1 = new UserDetails();
+
+    user1.name = "A R";
+    user1.age = 21;
+    user1.accountNo = 2511351613613616163616
+
+    return user1;
+
+
+}
+
+}
+
+/*
+
+
+*/
