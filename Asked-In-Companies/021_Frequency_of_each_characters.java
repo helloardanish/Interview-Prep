465078d9ff33921e1941109c247a895d67b1e12f
A R DANISH <helloardanish@gmail.com>
2026-09-19 13:50:33 +0530

    Create 021_Frequency_of_each_characters.java

---
+++ Asked-In-Companies/021_Frequency_of_each_characters.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      String name = "A R Danish";
+
+      // A-1, R-1, a-1,....
+
+      Map<Character, Integer> frequency = new HashMap<>();
+
+      for(int i=0; i<name.length(); i++){
+        char currChar = name.charAt(i);
+        if(frequency.containsKey(currChar)){
+          frequency.put(currChar, frequency.get(currChar)+1);
+        }else{
+          frequency.put(currChar, 1);
+        }
+      }
+
+      for(Map.Entry<Character, Integer> entry : frequency.entrySet()){
+        System.out.println(entry.getKey() +" : "+entry.getValue());
+      }
+
+    }
+}
